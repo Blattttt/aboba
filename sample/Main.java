0caaf8209c218cecf265c50bdb3bab62861b0758
Blattttt <77572968+Blattttt@users.noreply.github.com>
2022-04-09 15:51:48 +0300

    Update Main.java

--- sample/Main.java
+++ sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("firstWindow.fxml"));
         primaryStage.setTitle("Авторизация");
-        primaryStage.setScene(new Scene(root, 650, 400));
+        primaryStage.setScene(new Scene(root, 542, 336));
         primaryStage.show();
     }
 
