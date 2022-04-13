c4f8ee2077ebfcb46848789a5f8b3ba1c08675d2
Blattttt <77572968+Blattttt@users.noreply.github.com>
2022-04-13 17:27:55 +0300

    Update AdmWindow.java

--- sample/AdmWindow.java
+++ sample/AdmWindow.java
@@ -21,14 +21,17 @@ public class AdmWindow {
     @FXML private Button exitAdm1;
     @FXML private Button addUserAdm1;
     @FXML private Button changerolebtn;
-    @FXML private TableView<BD> medicineTable;
+    @FXML private TableView<BD> abonentsTable;
+    @FXML private TableView<BDaction> actionstable;
     @FXML private TableColumn<BD, String> Name;
+    @FXML private TableColumn<BDaction, String> Action;
     @FXML private TableColumn<BD, String> LastName;
     @FXML private TableColumn<BD, String> Age;
     @FXML private TableColumn<BD, String> UserRole;
     @FXML private TableColumn<BD, String> EmailAddress;
     @FXML private TableColumn<BD, String> Office;
-    private final ObservableList<BD> medicineData = FXCollections.observableArrayList();
+    private final ObservableList<BD> abonentsData = FXCollections.observableArrayList();
+    private final ObservableList<BDaction> actionsData = FXCollections.observableArrayList();
 
 
     @FXML
@@ -38,17 +41,26 @@ public class AdmWindow {
         Age.setCellValueFactory(new PropertyValueFactory<>("Age"));
         UserRole.setCellValueFactory(new PropertyValueFactory<>("UserRole"));
         EmailAddress.setCellValueFactory(new PropertyValueFactory<>("EmailAddress"));
-        Office.setCellValueFactory(new PropertyValueFactory<>("Office"));
+
+        Action.setCellValueFactory(new PropertyValueFactory<>("Action"));
 
         FXMLLoader loader = new FXMLLoader();
 
         tablerefresh();
 
-
-
-
-        exitAdm1.setOnAction(event ->
-                exitAdm1.getScene().getWindow().hide());
+        exitAdm1.setOnAction(event -> {
+                loader.setLocation(getClass().getResource("firstWindow.fxml"));
+                exitAdm1.getScene().getWindow().hide();
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Stage stage = new Stage();
+            assert root != null;
+            stage.setScene(new Scene(root));
+            stage.show();});
 
         addUserAdm1.setOnAction(event -> {
             loader.setLocation(getClass().getResource("adminWindow2.fxml"));
@@ -82,25 +94,36 @@ public class AdmWindow {
 
     }
     private void tablerefresh(){
-        medicineData.clear();
+        abonentsData.clear();
+        actionsData.clear();
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             try (Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.1.3:3306/a9",
                     "w9", "Qw12345678")) {
                 Statement statement = conn.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM abonents");
+                Statement statement2 = conn.createStatement();
+                ResultSet resultSet2 = statement2.executeQuery("SELECT * FROM schedule");
                 while(resultSet.next()){
-                        medicineData.add(new BD(
-                                resultSet.getString("FirstName"),
-                                resultSet.getString("LastName"),
-                                resultSet.getString("Birthdate"),
-                                resultSet.getString("RoleID"),
-                                resultSet.getString("Email"),
-                                resultSet.getString("OfficeID")));
+                    abonentsData.add(new BD(
+                                resultSet.getString("ФИО"),
+                                resultSet.getString("телефон"),
+                                resultSet.getString("Лицевой счет"),
+                                resultSet.getString("Услуги"),
+                                resultSet.getString("Номер договора")));
                 }
+                while(resultSet2.next()){
+                    uo uo = new uo(resultSet2.getString(2));
+                    actionsData.add(new BDaction(uo.getCheto()));
+                }
+
             }
-            if (!medicineData.isEmpty()){
-                medicineTable.setItems(medicineData);
+            if (!abonentsData.isEmpty()){
+                abonentsTable.setItems(abonentsData);
+            }
+
+            if (!actionsData.isEmpty()){
+                actionstable.setItems(actionsData);
             }
 
         }catch (Exception e){
